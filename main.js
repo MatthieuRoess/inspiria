ac6a3f2b71f7687e82a042d80ca7af57a34dee7d
Gabriel FACYLE <164531411+glabireFacyle@users.noreply.github.com>
2024-11-12 09:38:10 +0100

    "Updated code formatting and added newline at end of file"

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const scrollContainer = document.getElementById('horizontal-wrapper');
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollContainer = document.getElementById("horizontal-wrapper");
 
-    document.querySelectorAll('.next-button').forEach(function(button) {
-        button.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            const targetPosition = targetElement.offsetLeft;
-            scrollContainer.scrollTo({
-                left: targetPosition,
-                behavior: 'smooth'
-            });
-        });
+  document.querySelectorAll(".next-button").forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+      const targetPosition = targetElement.offsetLeft;
+      scrollContainer.scrollTo({
+        left: targetPosition,
+        behavior: "smooth",
+      });
     });
+  });
 });
